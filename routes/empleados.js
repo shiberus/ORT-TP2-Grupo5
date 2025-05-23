eb713a198c8fc4742fda3bef7941f07b8fd8c4e1
Santiago <santiagolopezcane@gmail.com>
2025-05-23 14:33:44 -0300

    actualizar imagen de perfil

--- routes/empleados.js
+++ routes/empleados.js
@@ -1,10 +1,13 @@
 import express from "express";
 const router = express.Router();
 import { verificarToken } from "../middlewares/authMiddleware.js";
+import { allowUpload } from "../middlewares/uploadMiddleware.js";
 
-import { CrearEmpleado, GetEmpleados } from "../controllers/empleadosController.js";
+import { CrearEmpleado, GetEmpleados, ActualizarProfilePic } from "../controllers/empleadosController.js";
 
-router.get("/", verificarToken, GetEmpleados);
+router.use(verificarToken);
+router.get("/", GetEmpleados);
 router.post("/", CrearEmpleado);
+router.put("/imagen", allowUpload.single('imagen'), ActualizarProfilePic);
 
 export default router;
