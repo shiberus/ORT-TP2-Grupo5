a093cf2ec7ff226bec7ec6415fe4cb6fd8243e6a
LOPEZ CANE SANTIAGO <41471665@comunidadort.edu.ar>
2025-05-23 14:33:44 -0300

    actualizar imagen de perfil

--- models/Empleado.js
+++ models/Empleado.js
@@ -4,7 +4,8 @@ const empleadoSchema = mongoose.Schema({
     nombre: { type: String, required: true},
     email: { type: String, required: true},
     password: { type: String, required: true},
-    rol: { type: String, required: true}
+    rol: { type: String, required: true},
+    profile_pic: { type: String},
 }, { timestamps: true});
 
 export default mongoose.model("Empleado", empleadoSchema);
--- utils/empleadoService.js
+++ utils/empleadoService.js
@@ -2,6 +2,7 @@ import Empleado from "../models/Empleado.js";
 import bcrypt from "bcryptjs";
 import ROLES from "../constants/roles.js";
 import CustomError from "./CustomError.js";
+import { uploadFile } from "./supabaseService.js";
 
 export const crearEmpleado = async (nombre, email, password, rol) => {
   if (!ROLES[rol]) {
@@ -16,7 +17,7 @@ export const crearEmpleado = async (nombre, email, password, rol) => {
     rol: ROLES[rol],
   };
 
-  const nuevoEmpleado = await Empleado.create(empleado).catch((error) => {
+  const nuevoEmpleado = await Empleado.create(empleado).catch(() => {
     throw new CustomError("Error al crear Empleado", 500);
   });
 
@@ -28,11 +29,26 @@ export const getEmpleados = async (query = {}) => {
     console.error("Error: ", error);
   });
   return empleados;
-}
+};
 
 export const getEmpleadoByEmail = async (email) => {
-  const empleado = await Empleado.findOne({email}).catch((error) => {
+  const empleado = await Empleado.findOne({ email }).catch((error) => {
     console.error("Error: ", error);
   });
+  return empleado;
+};
+
+export const actualizarProfilePic = async (empleadoId, imagen) => {
+  const imageUrl = await uploadFile(
+    `empleados/${empleadoId}/profilePic`,
+    imagen
+  );
+
+  const empleado = await Empleado.findByIdAndUpdate(
+    empleadoId,
+    { profile_pic: imageUrl },
+    { new: true }
+  );
+
   return empleado;
 };
