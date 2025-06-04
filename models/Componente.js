b9ae227ff55e38b2f0e638a420437f8966e47d55
clariredal <mclararedal@gmail.com>
2025-06-04 19:54:38 -0300

    cambios en Trabajo controller y modelo

--- models/Componente.js
+++ models/Componente.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const componenteSchema = mongoose.Schema({
-    nombre: {type: String, required: true}
+    nombre: {type: String, required: true},
+    costo: { type: Number, required: true }
 })
 
 export default mongoose.model("Componente", componenteSchema)
--- models/FacturaDetalle.js
+++ models/FacturaDetalle.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const facturaDetalleSchema = mongoose.Schema({
-  idFactura: { type: mongoose.Schema.Types.ObjectId, ref: "Factura", required: true },
-  idTrabajo: { type: mongoose.Schema.Types.ObjectId, ref: "Trabajo", required: true },
-  costoIndividual: { type: Number, required: true },
-  componentes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Componente" }] 
+    idFactura: { type: mongoose.Schema.Types.ObjectId, ref: "Factura", required: true },
+    idTrabajo: { type: mongoose.Schema.Types.ObjectId, ref: "Trabajo", required: true },
+    costoIndividual: { type: Number, required: true },
+    componentes: { type: mongoose.Schema.Types.ObjectId, ref: "Componente" }
 });
 
 export default mongoose.model("FacturaDetalle", facturaDetalleSchema)
