a97ff0c8f6a6f2251cf0c225237c5f2df4f673b7
clariredal <mclararedal@gmail.com>
2025-06-04 22:19:28 -0300

    factura & facturaDetalle modelos eliminados

--- models/Factura.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const facturaSchema = mongoose.Schema({
-    clienteId: { type: mongoose.Schema.Types.ObjectId, ref: "Cliente", required: true },
-    monto: {type: Number, required: true},
-    fecha: {type: Date, required: true}
-}, {timestamps: true})
-
-export default mongoose.model("Factura", facturaSchema)
