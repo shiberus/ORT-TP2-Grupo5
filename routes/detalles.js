485b8ffb49aea80652495cd9603462f85370fdb5
clariredal <mclararedal@gmail.com>
2025-06-25 18:16:28 -0300

    ultimos detalles

--- routes/detalles.js
+++ routes/detalles.js
@@ -5,12 +5,15 @@ import { verificarToken } from "../middlewares/authMiddleware.js";
 import { 
     GetDetalles, 
     GetDetalleById,
-    CrearDetalle
+    CrearDetalle,
+    GetDetalleWithTrabajoByIdCabecera
 } from "../controllers/detallesController.js";
 
 router.use(verificarToken);
-router.get("/", GetDetalles);
-router.get("/:id", GetDetalleById);
-router.post("/", CrearDetalle);
 
-export default router; 
+router.get("/", GetDetalles);
+router.get("/:id", GetDetalleById); 
+router.get("/comprobante/:idComprobanteCabecera", GetDetalleWithTrabajoByIdCabecera);
+router.post("/", CrearDetalle); 
+
+export default router;
